dcd862612080ecb156e98688eb63c76f4cc1994a
Dad S Wonkulah Jr <68390101+dadwonkulahjr@users.noreply.github.com>
2020-09-15 11:21:40 +0000

    Added a Person class file

---
+++ LinqTutorial/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqTutorial
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Person(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Id = {Id.ToString()}\nName = {Name}\nEmail = {Email}");
+        }
+    }
+}
--- LinqTutorial/Program.cs
+++ LinqTutorial/Program.cs
@@ -8,16 +8,37 @@ namespace LinqTutorial
     {
         static void Main(string[] args)
         {
-            IOrderedEnumerable<Student> students = Student.GetStudents().OrderBy(s => s.TotalMarks)
-                .ThenBy(x => x.Name).ThenBy(x => x.Id);
-            foreach (var item in students)
+            List<Employee> employees = (List<Employee>)Employee.GetEmployees();
+
+            var result = employees.ToLookup(x => x.JobTitle);
+            Console.WriteLine("Employees by Job Title:");
+            foreach (var kvp in result)
             {
-                Console.WriteLine(item.Id + " - " + item.Name + " - " + item.TotalMarks);
+                Console.WriteLine(kvp.Key);
+                foreach (var employee in result[kvp.Key])
+                {
+                    Console.WriteLine("\t" + employee.Name + "\t" + employee.JobTitle + "\t" + employee.City + "\t" + employee.Salary.ToString("c"));
+                }
             }
+            Console.WriteLine();
+            Console.WriteLine();
+            var resultSalary = employees.ToLookup(x => x.City, x => x);
+            Console.WriteLine("Employee By City:");
+            foreach (var kvp in resultSalary)
+            {
+                Console.WriteLine(kvp.Key);
+                foreach (var emp in resultSalary[kvp.Key])
+                {
+                    Console.WriteLine( "\t" + emp.Name + "\t" + emp.JobTitle + "\t" + emp.City + "\t" + emp.Salary.ToString("c"));
+                }
+            }
+          
+
             Console.ReadLine();
         }
 
     }
 
+    
 
 }
